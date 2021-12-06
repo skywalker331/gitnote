b0e6016fa0df8e5d564d3bb13b3dc42f496d6df5
WilliamGong <gtl119@live.com>
2021-12-06 15:40:12 +0800

    确认了全部翻译项

--- textedit/textedit_zh.ts
+++ textedit/textedit_zh.ts
@@ -6,47 +6,47 @@
     <message>
         <location filename="textedit.cpp" line="31"/>
         <source>Open file</source>
-        <translation type="unfinished">打开文件</translation>
+        <translation>打开文件</translation>
     </message>
     <message>
         <location filename="textedit.cpp" line="31"/>
         <location filename="textedit.cpp" line="58"/>
         <location filename="textedit.cpp" line="82"/>
         <source>Text files(*.txt)</source>
-        <translation type="unfinished">文本文件 (*.txt)</translation>
+        <translation>文本文件 (*.txt)</translation>
     </message>
     <message>
         <location filename="textedit.cpp" line="37"/>
         <source>Read file</source>
-        <translation type="unfinished">读取文件</translation>
+        <translation>读取文件</translation>
     </message>
     <message>
         <location filename="textedit.cpp" line="38"/>
         <source>Cannot open file: 
 %1</source>
-        <translation type="unfinished">不能打开文件：\n%1</translation>
+        <translation>不能打开文件：\n%1</translation>
     </message>
     <message>
         <location filename="textedit.cpp" line="50"/>
         <location filename="textedit.cpp" line="67"/>
         <source>Path</source>
-        <translation type="unfinished">路径</translation>
+        <translation>路径</translation>
     </message>
     <message>
         <location filename="textedit.cpp" line="51"/>
         <location filename="textedit.cpp" line="68"/>
         <source>You didn&apos;t select any file. </source>
-        <translation type="unfinished">没有选择任何文件。</translation>
+        <translation>没有选择任何文件。</translation>
     </message>
     <message>
         <location filename="textedit.cpp" line="57"/>
         <source>Select file</source>
-        <translation type="unfinished">选择文件</translation>
+        <translation>选择文件</translation>
     </message>
     <message>
         <location filename="textedit.cpp" line="81"/>
         <source>Create new file</source>
-        <translation type="unfinished">新建文件</translation>
+        <translation>新建文件</translation>
     </message>
 </context>
 <context>
@@ -64,47 +64,47 @@
     <message>
         <location filename="textedit.ui" line="53"/>
         <source>Help</source>
-        <translation type="unfinished">帮助</translation>
+        <translation>帮助</translation>
     </message>
     <message>
         <location filename="textedit.ui" line="63"/>
         <source>New</source>
-        <translation type="unfinished">新建</translation>
+        <translation>新建</translation>
     </message>
     <message>
         <location filename="textedit.ui" line="66"/>
         <source>Ctrl+N</source>
-        <translation type="unfinished">Ctrl+N</translation>
+        <translation>Ctrl+N</translation>
     </message>
     <message>
         <location filename="textedit.ui" line="71"/>
         <source>Open</source>
-        <translation type="unfinished">打开</translation>
+        <translation>打开</translation>
     </message>
     <message>
         <location filename="textedit.ui" line="74"/>
         <source>Ctrl+O</source>
-        <translation type="unfinished">Ctrl+O</translation>
+        <translation>Ctrl+O</translation>
     </message>
     <message>
         <location filename="textedit.ui" line="79"/>
         <source>Undo</source>
-        <translation type="unfinished">撤销</translation>
+        <translation>撤销</translation>
     </message>
     <message>
         <location filename="textedit.ui" line="84"/>
         <source>save</source>
-        <translation type="unfinished">保存</translation>
+        <translation>保存</translation>
     </message>
     <message>
         <location filename="textedit.ui" line="89"/>
         <source>Save</source>
-        <translation type="unfinished">保存</translation>
+        <translation>保存</translation>
     </message>
     <message>
         <location filename="textedit.ui" line="92"/>
         <source>Ctrl+S</source>
-        <translation type="unfinished">Ctrl+S</translation>
+        <translation>Ctrl+S</translation>
     </message>
 </context>
 </TS>
