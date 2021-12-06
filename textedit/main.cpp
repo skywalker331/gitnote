b5b33c55046f4aeeb8ab9a4ff45da7ab7a9959f9
WilliamGong <gtl119@live.com>
2021-12-06 15:31:03 +0800

    添加 i18n功能 用 Qt VS Tools 重新生成了 .pro 文件

--- textedit/main.cpp
+++ textedit/main.cpp
@@ -1,9 +1,14 @@
 #include "textedit.h"
 #include <QtWidgets/QApplication>
+#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QTranslator translator;
+    translator.load("textedit_zh.qm");
+    a.installTranslator(&translator);
     Textedit w;
     w.show();
     return a.exec();
---
+++ textedit/textedit_zh.ts
@@ -0,0 +1,110 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>Textedit</name>
+    <message>
+        <location filename="textedit.cpp" line="31"/>
+        <source>Open file</source>
+        <translation type="unfinished">打开文件</translation>
+    </message>
+    <message>
+        <location filename="textedit.cpp" line="31"/>
+        <location filename="textedit.cpp" line="58"/>
+        <location filename="textedit.cpp" line="82"/>
+        <source>Text files(*.txt)</source>
+        <translation type="unfinished">文本文件 (*.txt)</translation>
+    </message>
+    <message>
+        <location filename="textedit.cpp" line="37"/>
+        <source>Read file</source>
+        <translation type="unfinished">读取文件</translation>
+    </message>
+    <message>
+        <location filename="textedit.cpp" line="38"/>
+        <source>Cannot open file: 
+%1</source>
+        <translation type="unfinished">不能打开文件：\n%1</translation>
+    </message>
+    <message>
+        <location filename="textedit.cpp" line="50"/>
+        <location filename="textedit.cpp" line="67"/>
+        <source>Path</source>
+        <translation type="unfinished">路径</translation>
+    </message>
+    <message>
+        <location filename="textedit.cpp" line="51"/>
+        <location filename="textedit.cpp" line="68"/>
+        <source>You didn&apos;t select any file. </source>
+        <translation type="unfinished">没有选择任何文件。</translation>
+    </message>
+    <message>
+        <location filename="textedit.cpp" line="57"/>
+        <source>Select file</source>
+        <translation type="unfinished">选择文件</translation>
+    </message>
+    <message>
+        <location filename="textedit.cpp" line="81"/>
+        <source>Create new file</source>
+        <translation type="unfinished">新建文件</translation>
+    </message>
+</context>
+<context>
+    <name>TexteditClass</name>
+    <message>
+        <location filename="textedit.ui" line="39"/>
+        <source>File</source>
+        <translation>文件</translation>
+    </message>
+    <message>
+        <location filename="textedit.ui" line="47"/>
+        <source>Edit</source>
+        <translation>编辑</translation>
+    </message>
+    <message>
+        <location filename="textedit.ui" line="53"/>
+        <source>Help</source>
+        <translation type="unfinished">帮助</translation>
+    </message>
+    <message>
+        <location filename="textedit.ui" line="63"/>
+        <source>New</source>
+        <translation type="unfinished">新建</translation>
+    </message>
+    <message>
+        <location filename="textedit.ui" line="66"/>
+        <source>Ctrl+N</source>
+        <translation type="unfinished">Ctrl+N</translation>
+    </message>
+    <message>
+        <location filename="textedit.ui" line="71"/>
+        <source>Open</source>
+        <translation type="unfinished">打开</translation>
+    </message>
+    <message>
+        <location filename="textedit.ui" line="74"/>
+        <source>Ctrl+O</source>
+        <translation type="unfinished">Ctrl+O</translation>
+    </message>
+    <message>
+        <location filename="textedit.ui" line="79"/>
+        <source>Undo</source>
+        <translation type="unfinished">撤销</translation>
+    </message>
+    <message>
+        <location filename="textedit.ui" line="84"/>
+        <source>save</source>
+        <translation type="unfinished">保存</translation>
+    </message>
+    <message>
+        <location filename="textedit.ui" line="89"/>
+        <source>Save</source>
+        <translation type="unfinished">保存</translation>
+    </message>
+    <message>
+        <location filename="textedit.ui" line="92"/>
+        <source>Ctrl+S</source>
+        <translation type="unfinished">Ctrl+S</translation>
+    </message>
+</context>
+</TS>
