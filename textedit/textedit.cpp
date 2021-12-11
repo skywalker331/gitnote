2459cc044aedfa5b4c33ffb1ba3548e0d8227810
WilliamGong <gtl119@live.com>
2021-12-11 10:14:35 +0800

    添加了目录树 增加了“Open dir”选项 为"undo"增加了快捷键

--- textedit/textedit.cpp
+++ textedit/textedit.cpp
@@ -10,14 +10,24 @@ Textedit::Textedit(QWidget *parent) : QMainWindow(parent) {
 
   // connect signal and slot
   connect(ui.actionOpenFile, &QAction::triggered, this,
-          &Textedit::openFile);
+          static_cast<void (Textedit::*)()>(&Textedit::openFile));
   connect(ui.actionSaveFile, &QAction::triggered, this, &Textedit::saveFile);
   connect(ui.actionNewFile, &QAction::triggered, this, &Textedit::newFile);
+  connect(ui.actionOpenDir, &QAction::triggered, this, &Textedit::openDir);
+  connect(ui.treeViewDir, &QTreeView::doubleClicked, this,
+          static_cast<void (Textedit::*)(const QModelIndex &)>(&Textedit::openFile));
 
   // var init
   this->path = "Untitled.txt";
   this->info = QFileInfo(path);
   this->isFileDefault = true;
+  // Dir tree
+  modelDir.setRootPath(QDir::homePath());
+  ui.treeViewDir->setModel(&modelDir);
+  ui.treeViewDir->setRootIndex(modelDir.index(QDir::homePath()));
+  ui.treeViewDir->setColumnHidden(1, true);
+  ui.treeViewDir->setColumnHidden(2, true);
+  ui.treeViewDir->setColumnHidden(3, true);
   
 
   // set window
@@ -42,6 +52,11 @@ void Textedit::openFile() {
           this->path = path;
           this->info = QFileInfo(path);
           this->setWindowTitle(this->info.fileName());
+          // Dir tree
+          modelDir.setRootPath(info.dir().absolutePath());
+          ui.treeViewDir->setRootIndex(
+              modelDir.index(info.dir().absolutePath()));
+
           this->isFileDefault = false;
           QTextStream in(&file);
           ui.textEdit->setText(in.readAll());
@@ -50,6 +65,42 @@ void Textedit::openFile() {
           QMessageBox::warning(this, tr("Path"),
                                tr("You didn't select any file. "));
     }
+}
+
+void Textedit::openFile(const QModelIndex &index) {
+  if (!index.isValid()) {
+    QMessageBox::warning(this, tr("Open file"), tr("Error: invalid path. "));
+    return;
+  }
+  if (modelDir.fileInfo(index).isFile()) {
+    this->openFile(modelDir.fileInfo(index).absoluteFilePath());
+  }
+}
+
+void Textedit::openFile(QString path) {
+  if (!path.isEmpty()) {
+    QFile file(path);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+      // Warning: cannot open file
+      QMessageBox::warning(this, tr("Read file"),
+                           tr("Cannot open file: \n%1").arg(path));
+      return;
+    }
+
+    this->path = path;
+    this->info = QFileInfo(path);
+    this->setWindowTitle(this->info.fileName());
+    // Dir tree
+    modelDir.setRootPath(info.dir().absolutePath());
+    ui.treeViewDir->setRootIndex(modelDir.index(info.dir().absolutePath()));
+
+    this->isFileDefault = false;
+    QTextStream in(&file);
+    ui.textEdit->setText(in.readAll());
+    file.close();
+  } else {
+    QMessageBox::warning(this, tr("Path"), tr("You didn't select any file. "));
+  }
 }
 
 void Textedit::saveFile() {
@@ -88,5 +139,17 @@ void Textedit::newFile() {
 
     // empty textEdit
     ui.textEdit->setText(NULL);
+  }
+}
+
+void Textedit::openDir() {
+  QString dir =
+      QFileDialog::getExistingDirectory(this, tr("Open Directory"), ".");
+  if (!dir.isEmpty()) {
+    modelDir.setRootPath(dir);
+    ui.treeViewDir->setRootIndex(modelDir.index(dir));
+  } else {
+    QMessageBox::warning(this, tr("Open directory"),
+                         tr("Can not open this directory: \n%1").arg(dir));
   }
 }
--- textedit/textedit_zh.ts
+++ textedit/textedit_zh.ts
@@ -4,107 +4,149 @@
 <context>
     <name>Textedit</name>
     <message>
-        <location filename="textedit.cpp" line="31"/>
+        <location filename="textedit.cpp" line="41"/>
+        <location filename="textedit.cpp" line="72"/>
         <source>Open file</source>
         <translation>打开文件</translation>
     </message>
     <message>
-        <location filename="textedit.cpp" line="31"/>
-        <location filename="textedit.cpp" line="58"/>
-        <location filename="textedit.cpp" line="82"/>
+        <location filename="textedit.cpp" line="41"/>
+        <location filename="textedit.cpp" line="109"/>
+        <location filename="textedit.cpp" line="133"/>
         <source>Text files(*.txt)</source>
         <translation>文本文件 (*.txt)</translation>
     </message>
     <message>
-        <location filename="textedit.cpp" line="37"/>
+        <location filename="textedit.cpp" line="47"/>
+        <location filename="textedit.cpp" line="85"/>
         <source>Read file</source>
         <translation>读取文件</translation>
     </message>
     <message>
-        <location filename="textedit.cpp" line="38"/>
+        <location filename="textedit.cpp" line="48"/>
+        <location filename="textedit.cpp" line="86"/>
         <source>Cannot open file: 
 %1</source>
-        <translation>不能打开文件：\n%1</translation>
+        <translation>不能打开文件：
+%1</translation>
     </message>
     <message>
-        <location filename="textedit.cpp" line="50"/>
-        <location filename="textedit.cpp" line="67"/>
+        <location filename="textedit.cpp" line="65"/>
+        <location filename="textedit.cpp" line="102"/>
+        <location filename="textedit.cpp" line="118"/>
         <source>Path</source>
         <translation>路径</translation>
     </message>
     <message>
-        <location filename="textedit.cpp" line="51"/>
-        <location filename="textedit.cpp" line="68"/>
+        <location filename="textedit.cpp" line="66"/>
+        <location filename="textedit.cpp" line="102"/>
+        <location filename="textedit.cpp" line="119"/>
         <source>You didn&apos;t select any file. </source>
         <translation>没有选择任何文件。</translation>
     </message>
     <message>
-        <location filename="textedit.cpp" line="57"/>
+        <location filename="textedit.cpp" line="72"/>
+        <source>Error: invalid path. </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="textedit.cpp" line="108"/>
         <source>Select file</source>
         <translation>选择文件</translation>
     </message>
     <message>
-        <location filename="textedit.cpp" line="81"/>
+        <location filename="textedit.cpp" line="132"/>
         <source>Create new file</source>
         <translation>新建文件</translation>
+    </message>
+    <message>
+        <location filename="textedit.cpp" line="147"/>
+        <source>Open Directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="textedit.cpp" line="152"/>
+        <source>Open directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="textedit.cpp" line="153"/>
+        <source>Can not open this directory: 
+%1</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>TexteditClass</name>
     <message>
-        <location filename="textedit.ui" line="39"/>
+        <location filename="textedit.ui" line="54"/>
         <source>File</source>
         <translation>文件</translation>
     </message>
     <message>
-        <location filename="textedit.ui" line="47"/>
+        <location filename="textedit.ui" line="63"/>
         <source>Edit</source>
         <translation>编辑</translation>
     </message>
     <message>
-        <location filename="textedit.ui" line="53"/>
+        <location filename="textedit.ui" line="69"/>
         <source>Help</source>
         <translation>帮助</translation>
     </message>
     <message>
-        <location filename="textedit.ui" line="63"/>
+        <location filename="textedit.ui" line="79"/>
         <source>New</source>
         <translation>新建</translation>
     </message>
     <message>
-        <location filename="textedit.ui" line="66"/>
+        <location filename="textedit.ui" line="82"/>
         <source>Ctrl+N</source>
         <translation>Ctrl+N</translation>
     </message>
     <message>
-        <location filename="textedit.ui" line="71"/>
+        <location filename="textedit.ui" line="87"/>
         <source>Open</source>
         <translation>打开</translation>
     </message>
     <message>
-        <location filename="textedit.ui" line="74"/>
+        <location filename="textedit.ui" line="90"/>
         <source>Ctrl+O</source>
         <translation>Ctrl+O</translation>
     </message>
     <message>
-        <location filename="textedit.ui" line="79"/>
+        <location filename="textedit.ui" line="95"/>
         <source>Undo</source>
         <translation>撤销</translation>
     </message>
     <message>
-        <location filename="textedit.ui" line="84"/>
+        <location filename="textedit.ui" line="98"/>
+        <source>Ctrl+U</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="textedit.ui" line="103"/>
         <source>save</source>
         <translation>保存</translation>
     </message>
     <message>
-        <location filename="textedit.ui" line="89"/>
+        <location filename="textedit.ui" line="108"/>
         <source>Save</source>
         <translation>保存</translation>
     </message>
     <message>
-        <location filename="textedit.ui" line="92"/>
+        <location filename="textedit.ui" line="111"/>
         <source>Ctrl+S</source>
         <translation>Ctrl+S</translation>
+    </message>
+    <message>
+        <location filename="textedit.ui" line="116"/>
+        <source>Open dir</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="textedit.ui" line="119"/>
+        <source>Ctrl+Shift+O</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
