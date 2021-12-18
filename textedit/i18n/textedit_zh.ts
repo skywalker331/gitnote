23dacc1653ed8ac3532815f8ab5762a0cc48743c
WilliamGong <gtl119@live.com>
2021-12-18 23:39:57 +0800

    移动语言文件位置

---
+++ textedit/i18n/textedit_zh.ts
@@ -0,0 +1,88 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>Textedit</name>
+    <message>
+        <source>Open file</source>
+        <translation type="vanished">打开文件</translation>
+    </message>
+    <message>
+        <source>Text files(*.txt)</source>
+        <translation type="vanished">文本文件 (*.txt)</translation>
+    </message>
+    <message>
+        <source>Read file</source>
+        <translation type="vanished">读取文件</translation>
+    </message>
+    <message>
+        <source>Cannot open file: 
+%1</source>
+        <translation type="vanished">不能打开文件：
+%1</translation>
+    </message>
+    <message>
+        <source>Path</source>
+        <translation type="vanished">路径</translation>
+    </message>
+    <message>
+        <source>You didn&apos;t select any file. </source>
+        <translation type="vanished">没有选择任何文件。</translation>
+    </message>
+    <message>
+        <source>Select file</source>
+        <translation type="vanished">选择文件</translation>
+    </message>
+    <message>
+        <source>Create new file</source>
+        <translation type="vanished">新建文件</translation>
+    </message>
+</context>
+<context>
+    <name>TexteditClass</name>
+    <message>
+        <source>File</source>
+        <translation type="vanished">文件</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation type="vanished">编辑</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation type="vanished">帮助</translation>
+    </message>
+    <message>
+        <source>New</source>
+        <translation type="vanished">新建</translation>
+    </message>
+    <message>
+        <source>Ctrl+N</source>
+        <translation type="vanished">Ctrl+N</translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation type="vanished">打开</translation>
+    </message>
+    <message>
+        <source>Ctrl+O</source>
+        <translation type="vanished">Ctrl+O</translation>
+    </message>
+    <message>
+        <source>Undo</source>
+        <translation type="vanished">撤销</translation>
+    </message>
+    <message>
+        <source>save</source>
+        <translation type="vanished">保存</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation type="vanished">保存</translation>
+    </message>
+    <message>
+        <source>Ctrl+S</source>
+        <translation type="vanished">Ctrl+S</translation>
+    </message>
+</context>
+</TS>
